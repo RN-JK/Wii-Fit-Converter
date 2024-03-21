bdaf69d4b1239aec87db02840225d677657ddd45
JackLSummer15 <62530870+JackLSummer15@users.noreply.github.com>
2024-03-21 18:28:27 -0400

    Rhythm Kung Fu (.bin)

--- WiiFit.py
+++ WiiFit.py
@@ -1,5 +1,41 @@
 import os, struct, json
 
+class RhythmKungFu:
+    def Decompile(input,output):
+        running=True
+        data=[]
+        b=open(input,'rb')
+        
+        while running==True:
+            duration=b.read(2)
+            if duration==b'\xFF\xFF' or duration==b'':
+                break
+            else:
+                array=[]
+                array.append(struct.unpack('>H',duration)[0])
+                array.append(struct.unpack('>B',b.read(1))[0])
+                array.append(struct.unpack('>B',b.read(1))[0])
+                data.append(array)
+
+        json.dump(data,open(output,'w'))
+    
+    def Compile(input,output,type):
+        data=json.load(open(input))
+        b=open(output,'wb')
+        for d in data:
+            b.write(struct.pack('>H',d[0]))
+            b.write(struct.pack('>B',d[1]))
+            b.write(struct.pack('>B',d[2]))
+
+        if type==0:
+            for _ in range(28):
+                b.write(b'\xFF')
+        if type==1:
+            for _ in range(24):
+                b.write(b'\xFF')
+        
+        b.close()
+
 class RhythmParade:
     def Decompile(input,output):
         data=[]
