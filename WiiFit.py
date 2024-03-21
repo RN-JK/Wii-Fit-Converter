c03be2a66d25fb3efa2f3cc401ad12c0fc49c31e
JackLSummer15 <62530870+JackLSummer15@users.noreply.github.com>
2024-03-20 23:20:41 -0400

    Basic/Advanced Step Tmls (.bin)

--- WiiFit.py
+++ WiiFit.py
@@ -22,3 +22,23 @@ class RhythmParade:
                 b.write(struct.pack('>B',a))
 
         b.close()
+
+class Step:
+    def Decompile(input,output):
+        data=[]
+        b=open(input,'rb')
+        count=struct.unpack('>H',b.read(2))[0]
+        for _ in range(count):
+            b.read(1) #00
+            data.append(struct.unpack('>B',b.read(1))[0])
+
+        json.dump(data,open(output,'w'))
+
+    def Compile(input,output):
+        data=json.load(open(input))
+        b=open(output,'wb')
+        b.write(struct.pack('>H',len(data)))
+        for d in data:
+            b.write(b'\x00'+struct.pack('>B',d))
+
+        b.close()
