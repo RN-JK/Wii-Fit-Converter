e7d73dce0f30d29b5f913dd11f07c33a0c183066
JackLSummer15 <62530870+JackLSummer15@users.noreply.github.com>
2024-03-21 19:51:53 -0400

    .tag files

--- WiiFit.py
+++ WiiFit.py
@@ -1,5 +1,39 @@
 import os, struct, json
 
+class Tag:
+    def Decompile(input,output):
+        data=[]
+        b=open(input,'rb')
+        b.read(4) #BOBJ
+        count=struct.unpack('>I',b.read(4))[0]
+        for _ in range(count):
+            array=[]
+            name=b.read(8).replace(b'\x00',b'').decode('utf-8')
+            array.append(name)
+            for __ in range(10):
+                array.append(struct.unpack('>f',b.read(4))[0])
+
+            data.append(array)
+
+        json.dump(data,open(output,'w'))
+
+    def Compile(input,output):
+        data=json.load(open(input))
+        b=open(output,'wb')
+        b.write(b'\x42\x4F\x42\x4A') #BOBJ
+        b.write(struct.pack('>I',len(data)))
+        namebyte=b'\x00\x00\x00\x00\x00\x00\x00\x00'
+        for d in data:
+            arraycounter=1
+            b.write(namebyte.replace(namebyte[:len(d[0])],d[0].encode('utf-8'), 1))
+            for __ in range(10):
+                b.write(struct.pack('>f',d[arraycounter]))
+                arraycounter+=1
+        
+        for _ in range(8):
+            b.write(b'\xFF')
+        b.close()
+
 class RhythmKungFu:
     def Decompile(input,output):
         running=True
